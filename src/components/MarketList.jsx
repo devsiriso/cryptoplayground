ad27a93acfe3ccabbf3ae31a21bed4c4b1972249
Sam <svschaik.main@gmail.com>
2021-09-24 11:34:37 +0200

    made marketlist scale

--- src/components/MarketList.jsx
+++ src/components/MarketList.jsx
@@ -1,52 +1,68 @@
-import { Divider, Flex, HStack, Image, List, Text } from '@chakra-ui/react';
+import {
+  Divider,
+  Flex,
+  HStack,
+  Image,
+  List,
+  Text,
+  Heading,
+  Container,
+} from '@chakra-ui/react';
 import React from 'react';
 import { roundToTwo } from '../Util';
 import { BuyModal } from './BuyModal';
 
-export const MarketList = ({ marketCoins, getColor, purchaseCoin, money }) => {
+export const MarketList = ({
+  marketCoins,
+  getColor,
+  purchaseCoin,
+  money,
+  spawnToast,
+}) => {
   return (
     <List
-      width="95%"
-      style={{
-        maxHeight: '40%',
-        overflowY: 'auto',
-        overflowX: 'hidden',
-        overflowInline: 'hidden',
-      }}
-    >
-      {marketCoins &&
-        marketCoins.map((coin, i) => (
-          <Flex direction="column">
-            <Flex key={coin.id} justifyContent="space-between">
-              <HStack>
-                <Image src={coin.logo_url} boxSize="30px" />
-                <Flex direction="column" style={{ textAlign: 'left' }}>
-                  <Text>{coin.id}</Text>
-                  <Text fontSize="sm">{coin.name}</Text>
-                </Flex>
-              </HStack>
+        width="95%"
+        style={{
+          
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          overflowInline: 'hidden',
+        }}
+      >
+        {marketCoins &&
+          marketCoins.map((coin, i) => (
+            <Flex direction="column">
+              <Flex key={coin.id} justifyContent="space-between">
+                <HStack>
+                  <Image src={coin.logo_url} boxSize="30px" />
+                  <Flex direction="column" style={{ textAlign: 'left' }}>
+                    <Text>{coin.id}</Text>
+                    <Text fontSize="sm">{coin.name}</Text>
+                  </Flex>
+                </HStack>
 
-              <HStack>
-                <Flex direction="column" style={{ textAlign: 'right' }}>
-                  <Text>${roundToTwo(coin.price)}</Text>
-                  <Text
-                    fontSize="sm"
-                    color={getColor(coin['30d'].price_change_pct)}
-                  >
-                    {roundToTwo(coin['30d'].price_change_pct)}%
-                  </Text>
-                </Flex>
-                <BuyModal
-                  showModalButtonText="+"
-                  coin={coin}
-                  money={money}
-                  purchaseCoin={purchaseCoin}
-                />
-              </HStack>
+                <HStack>
+                  <Flex direction="column" style={{ textAlign: 'right' }}>
+                    <Text>${roundToTwo(coin.price)}</Text>
+                    <Text
+                      fontSize="sm"
+                      color={getColor(coin['1d'].price_change_pct)}
+                    >
+                      {roundToTwo(coin['1d'].price_change)}({roundToTwo(coin['1d'].price_change_pct)}%)
+                    </Text>
+                  </Flex>
+                  <BuyModal
+                    showModalButtonText="+"
+                    coin={coin}
+                    money={money}
+                    purchaseCoin={purchaseCoin}
+                    spawnToast={spawnToast}
+                  />
+                </HStack>
+              </Flex>
+              <Divider m={2} />
             </Flex>
-            <Divider m={2} />
-          </Flex>
-        ))}
-    </List>
+          ))}
+      </List>
   );
 };
