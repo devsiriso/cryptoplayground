5b4969381ffa20a9b219eea74a7c27a11772a836
Sam <svschaik.main@gmail.com>
2021-09-28 19:31:59 +0200

    Added infobutton and lightbutton (to header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,22 @@
-import { Flex, Heading, Stack } from "@chakra-ui/react";
+import { Flex, Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import { InfoButton } from './InfoButton';
 import { Logo } from './Logo';
 
 export const Header = () => {
-    return (
-        <Flex justify="center" height="10%">
-            {/* <Heading size="md">CryptoPlayground</Heading> */}
-            {/* <Logo /> */}
-            {/* <ColorModeSwitcher/> */}
+  return (
+    <Flex justify="center" height="10%" justifyContent="space-between" width="100%">
+      <ColorModeSwitcher/>
+      {/* <Heading size="md">CryptoPlayground</Heading> */}
+      {/* <Logo /> */}
+      {/* <ColorModeSwitcher/> */}
 
-            <Stack direction="row" spacing="4" align="center" justify="space-between">
-
-          <Logo />
-          <Heading size="lg" >CryptoPlayground</Heading>
-   
-        </Stack>
-        </Flex>
-    )
-}
-
+      <Stack direction="row" spacing="4" align="center" justify="space-between">
+        <Logo />
+        <Heading size="lg">CryptoPlayground</Heading>
+      </Stack>
+      <InfoButton/>
+    </Flex>
+  );
+};
---
+++ src/components/InfoButton.jsx
@@ -0,0 +1,32 @@
+import { InfoIcon } from '@chakra-ui/icons';
+import {
+    Button, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverFooter, PopoverHeader, PopoverTrigger
+} from '@chakra-ui/react';
+
+export const InfoButton = () => {
+  return (
+    <Popover>
+         {({ isOpen, onClose }) => (
+         <>
+            <PopoverTrigger>
+        <IconButton variant="ghost" icon={<InfoIcon />} />
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Welcome to CryptoPlayground</PopoverHeader>
+        <PopoverBody>
+          Click on the '+' next to a specific coin to buy it. 
+          <br/>Click on one of the coins in your portfolio to sell it. 
+          <br/>You can add more money to your bank by pressing the green button. 
+          <br/>You can sell all your coins and claim your ride to the moon by pressing the pink button.
+        </PopoverBody>
+        <PopoverFooter>
+          <Button width="100%" colorScheme="teal" onClick={onClose}>
+              Start your trading adventure!
+          </Button>
+        </PopoverFooter>
+      </PopoverContent></>)}
+    </Popover>
+  );
+};
